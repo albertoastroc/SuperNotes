ae66c5dc02969f8963b5564d5084615092b6232a
Alberto Castro <jaycastro100@gmail.com>
2023-08-09 22:58:37 -0400

    pause

--- app/src/main/java/com/gmail/pentominto/us/supernotes/screens/noteeditscreen/composables/NoteEditScreen.kt
+++ app/src/main/java/com/gmail/pentominto/us/supernotes/screens/noteeditscreen/composables/NoteEditScreen.kt
@@ -1,56 +1,33 @@
 package com.gmail.pentominto.us.supernotes.screens.noteeditscreen.composables
 
-import android.app.DatePickerDialog
-import android.app.TimePickerDialog
-import android.content.Context
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.BottomSheetValue
-import androidx.compose.material.Button
 import androidx.compose.material.DrawerValue
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.material.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import com.gmail.pentominto.us.supernotes.activities.mainactivity.navhelpers.composables.NavigationDrawerOptions
 import com.gmail.pentominto.us.supernotes.screens.noteeditscreen.NoteEditScreenViewModel
 import kotlinx.coroutines.launch
-import java.util.Calendar
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -61,11 +38,7 @@ fun NoteEditScreen(
 
 ) {
 
-    val shouldShowDateTimePickerDialogue =  remember { mutableStateOf(false) }
-
     val lifeCycleOwner = LocalLifecycleOwner.current.lifecycle
-
-    val context = LocalContext.current
 
     val coroutineScope = rememberCoroutineScope()
 
@@ -129,16 +102,6 @@ fun NoteEditScreen(
                     )
             ) {
 
-//                if (shouldShowDateTimePickerDialogue.value){
-//
-//                    DateTimePickerDialogue(
-//                        context = context,
-//                        shouldShowDateTimePickerDialogue = shouldShowDateTimePickerDialogue
-//                    )
-//
-//                }
-
-
                 TitleAndMenuCard(
                     customTextSelectionColors = customTextSelectionColors,
                     noteState = noteState,
@@ -170,130 +133,4 @@ fun NoteEditScreen(
             }
         }
     )
-}
-
-@Composable
-fun DateTimePickerDialogue(
-    context : Context,
-    shouldShowDateTimePickerDialogue : MutableState<Boolean>,
-    scheduleReminder : (Context, Long) -> Unit
-){
-
-    val calendar = Calendar.getInstance()
-
-    val datePicker = DatePickerDialog(
-        context,
-        { _, year, month, dayOfMonth ->
-            calendar.set(year, month, dayOfMonth)
-        },
-        calendar[Calendar.YEAR],
-        calendar[Calendar.MONTH],
-        calendar[Calendar.DAY_OF_MONTH]
-    )
-
-    val timePicker = TimePickerDialog(
-        context,
-        { view, selectedHour: Int, selectedMinute: Int ->
-            calendar.set(
-                datePicker.datePicker.year,
-                datePicker.datePicker.month,
-                datePicker.datePicker.dayOfMonth,
-                selectedHour,
-                selectedMinute
-            )
-        },
-        calendar[Calendar.HOUR_OF_DAY],
-        calendar[Calendar.MINUTE],
-        false
-    )
-
-    Dialog(onDismissRequest = {
-        shouldShowDateTimePickerDialogue.value = false
-       }
-    ) {
-
-        Surface(
-            shape = RoundedCornerShape(16.dp),
-            color = Color.White
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .heightIn(),
-                contentAlignment = Alignment.Center){
-
-                Column() {
-
-
-                    Spacer(modifier = Modifier.height(12.dp))
-
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text(
-                            modifier = Modifier.padding(start = 16.dp),
-                            text = "9:00 AM",
-                            color = Color.Black
-                        )
-
-                        IconButton(onClick = {
-
-                            timePicker.show()
-
-                        }) {
-                            Icon(Icons.Default.Edit,
-                                contentDescription = "icon",
-                                tint = Color.Black
-                            )
-                        }
-                    }
-
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text(
-                            modifier = Modifier.padding(start = 16.dp),
-                            text = "12/01/23",
-                            color = Color.Black
-                        )
-
-                        IconButton(onClick = {
-                            datePicker.show()
-                        }) {
-                            Icon(Icons.Default.Edit,
-                                contentDescription = "icon",
-                                tint = Color.Black
-                            )
-                        }
-                    }
-
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        Button(onClick = {
-
-                            scheduleReminder(context, calendar.timeInMillis)
-
-                        }) {
-                            Text(text = "Set reminder")
-                        }
-                    }
-
-                    Spacer(modifier = Modifier.height(8.dp))
-
-
-                }
-
-            }
-        }
-
-    }
-
-
 }
--- app/src/main/java/com/gmail/pentominto/us/supernotes/screens/noteeditscreen/composables/TitleCard.kt
+++ app/src/main/java/com/gmail/pentominto/us/supernotes/screens/noteeditscreen/composables/TitleCard.kt
@@ -2,7 +2,9 @@ package com.gmail.pentominto.us.supernotes.screens.noteeditscreen.composables
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.app.NotificationManager
+import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -14,28 +16,39 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.BottomSheetScaffoldState
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.SnackbarResult
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -52,11 +65,14 @@ import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.core.app.NotificationManagerCompat
 import com.gmail.pentominto.us.supernotes.R
 import com.gmail.pentominto.us.supernotes.screens.noteeditscreen.NoteEditState
 import com.gmail.pentominto.us.supernotes.utility.NoRippleInteractionSource
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 @OptIn(
     ExperimentalMaterialApi::class
@@ -71,8 +87,6 @@ fun TitleAndMenuCard(
     bottomSheetScaffoldState: BottomSheetScaffoldState
 ) {
     val context = LocalContext.current
-
-
 
     Card(
         modifier = Modifier,
@@ -145,10 +159,8 @@ fun TitleAndMenuCard(
 private fun NoteEditMenu(
     noteState: NoteEditState,
     scheduleReminder : (Context, Long) -> Unit,
-    //showReminderSchedulerDialog: () -> Unit,
     context: Context,
     bottomSheetScaffoldState: BottomSheetScaffoldState,
-    //launcher: ManagedActivityResultLauncher<String, Boolean>
 ) {
 
     val shouldShowDateTimePickerDialogue =  remember { mutableStateOf(false) }
@@ -255,133 +267,14 @@ private fun NoteEditMenu(
                         bottomSheetScaffoldState,
                         launcher
                     )
-
                 }
-
-
-
-
-//                coroutineScope.launch {
-//                    requestNotificationPermission(
-//                        packageManager,
-//                        context,
-//                        showReminderSchedulerDialog,
-//                        bottomSheetScaffoldState,
-//                        launcher
-//                    )
-//                }
-//
-//                dropDownMenuExpanded = false
+                dropDownMenuExpanded = false
             }) {
                 Text(text = "Set reminder")
             }
         }
     }
 }
-//@Composable
-//fun DateTimePickerDialogue(context : Context){
-//
-//    val calendar = Calendar.getInstance()
-//
-//    val datePicker = DatePickerDialog(
-//        context,
-//        { _, year, month, dayOfMonth ->
-//            calendar.set(year, month, dayOfMonth)
-//        },
-//        calendar[Calendar.YEAR],
-//        calendar[Calendar.MONTH],
-//        calendar[Calendar.DAY_OF_MONTH]
-//    )
-//
-//    val timePicker = TimePickerDialog(
-//        context,
-//        { view, selectedHour: Int, selectedMinute: Int ->
-//            calendar.set(
-//                datePicker.datePicker.year,
-//                datePicker.datePicker.month,
-//                datePicker.datePicker.dayOfMonth,
-//                selectedHour,
-//                selectedMinute
-//            )
-//        },
-//        calendar[Calendar.HOUR_OF_DAY],
-//        calendar[Calendar.MINUTE],
-//        false
-//    )
-//
-//    Dialog(onDismissRequest = { }) {
-//
-//        Surface(
-//            shape = RoundedCornerShape(16.dp),
-//            color = Color.White
-//        ) {
-//            Box(
-//                modifier = Modifier,
-//                contentAlignment = Alignment.Center){
-//
-//                Row() {
-//                    Text(text = "First row")
-//                }
-//
-//                Row() {
-//                    Text(text = "Second row")
-//                }
-//
-//            }
-//        }
-//
-//    }
-//
-//}
-
-//private fun showDateTimePickerDialogues(context: Context, setAlarm: (Context, Long) -> Unit): () -> Unit {
-//
-//    val calendar = Calendar.getInstance()
-//
-//    val datePicker = DatePickerDialog(
-//        context,
-//        { _, year, month, dayOfMonth ->
-//            calendar.set(year, month, dayOfMonth)
-//        },
-//        calendar[Calendar.YEAR],
-//        calendar[Calendar.MONTH],
-//        calendar[Calendar.DAY_OF_MONTH]
-//    )
-//
-//    val timePicker = TimePickerDialog(
-//        context,
-//        { view, selectedHour: Int, selectedMinute: Int ->
-//            calendar.set(
-//                datePicker.datePicker.year,
-//                datePicker.datePicker.month,
-//                datePicker.datePicker.dayOfMonth,
-//                selectedHour,
-//                selectedMinute
-//            )
-//        },
-//        calendar[Calendar.HOUR_OF_DAY],
-//        calendar[Calendar.MINUTE],
-//        false
-//    )
-//
-//    val showAlarmDialogs = {
-//        datePicker.show()
-//        datePicker.setOnDateSetListener { _, _, _, _ ->
-//
-//            timePicker.show()
-//            timePicker.setOnDismissListener {
-//                setAlarm(
-//                    context,
-//                    calendar.timeInMillis
-//                )
-//            }
-//        }
-//    }
-//
-//    datePicker.datePicker.minDate = calendar.timeInMillis
-//    return showAlarmDialogs
-//}
-
 
 @OptIn(ExperimentalMaterialApi::class)
 private suspend fun requestNotificationPermission(
@@ -436,5 +329,140 @@ private suspend fun requestNotificationPermission(
         }
     } else {
         launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+    }
+}
+
+@Composable
+fun DateTimePickerDialogue(
+    context : Context,
+    shouldShowDateTimePickerDialogue : MutableState<Boolean>,
+    scheduleReminder : (Context, Long) -> Unit
+){
+
+    val calendar = Calendar.getInstance()
+
+    val dateFormatter = SimpleDateFormat("MM-dd-yyyy")
+    val date = dateFormatter.format(calendar.time)
+
+    val timeFormatter = SimpleDateFormat("hh:mm aa")
+    val time = timeFormatter.format(calendar.time)
+
+    val datePicker = DatePickerDialog(
+        context,
+        { _, year, month, dayOfMonth ->
+            calendar.set(year, month, dayOfMonth)
+        },
+
+        calendar[Calendar.YEAR],
+        calendar[Calendar.MONTH],
+        calendar[Calendar.DAY_OF_MONTH],
+    )
+
+    val timePicker = TimePickerDialog(
+        context,
+        { view, selectedHour: Int, selectedMinute: Int ->
+            calendar.set(
+                datePicker.datePicker.year,
+                datePicker.datePicker.month,
+                datePicker.datePicker.dayOfMonth,
+                selectedHour,
+                selectedMinute
+            )
+
+        },
+        calendar[Calendar.HOUR_OF_DAY],
+        calendar[Calendar.MINUTE],
+        false
+    )
+
+    val dateState =  remember { mutableStateOf(date) }
+    val timeState =  remember { mutableStateOf(time) }
+
+    Dialog(onDismissRequest = {
+        shouldShowDateTimePickerDialogue.value = false
+    }) {
+
+        Surface(
+            shape = RoundedCornerShape(16.dp),
+            color = Color.White
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(),
+                contentAlignment = Alignment.Center){
+
+                Column() {
+
+
+                    Spacer(modifier = Modifier.height(12.dp))
+
+                    //time
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(start = 16.dp),
+                            text = dateState.value,
+                            color = Color.Black
+                        )
+
+                        IconButton(onClick = {
+
+                            datePicker.show()
+
+                        }) {
+                            Icon(Icons.Default.Edit,
+                                contentDescription = "icon",
+                                tint = Color.Black
+                            )
+                        }
+                    }
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(start = 16.dp),
+                            text = timeState.value,
+                            color = Color.Black
+                        )
+
+                        IconButton(onClick = {
+
+                            timePicker.show()
+
+                        }) {
+                            Icon(Icons.Default.Edit,
+                                contentDescription = "icon",
+                                tint = Color.Black
+                            )
+                        }
+                    }
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Button(onClick = {
+
+                            scheduleReminder(context, calendar.timeInMillis)
+                            shouldShowDateTimePickerDialogue.value = false
+
+                        }) {
+                            Text(text = "Set reminder")
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                }
+            }
+        }
     }
 }
